ff92469a56c14bb5b21b7c711b6ecf175b992cd9
Nina Kim <ninakim0504@hotmail.com>
2020-10-12 12:57:37 -0400

    newfiles

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const db =  require('./models')
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static('./public'));
 
 let todoList = [
@@ -16,69 +16,100 @@ let todoList = [
 
 // GET /api/todos
 app.get('/api/todos', (req, res) => {
-  res.json(todoList);
+  db.Todo.findAll()
+    .then((todos) => {
+      res.json(todos);
+    })
+    .catch((error => {
+      console.error(error);
+      res.status(500).json({error: 'A Database Error Occurred' });
+    }))
 });
 
 // GET /api/todos/:id
 app.get('/api/todos/:id', (req, res) => {
-  const todo =
-    todoList.find((todo) => {
-      return todo.id === Number.parseInt(req.params.id);
-    }) || {};
-  const status = Object.keys(todo).length ? 200 : 404;
-  res.status(status).json(todo);
+  const{ id } = req.params;
+  db.Todo.findByPk(id)
+  .then((todo) => {
+    if (!todo) {
+      res.status(404).json({ error: `Could not find Todo with id: ${id}`})
+      return;
+    }
+    res.json(todo);
+  })
+  .catch((error => {
+    console.error(error);
+    res.status(500).json({error: 'A Database Error Occurred' });
+  }))
 });
 
 // POST /api/todos
 app.post('/api/todos', (req, res) => {
-  if (!req.body || !req.body.todo) {
+  if (!req.body || !req.body.name) {
     res.status(400).json({
       error: 'Provide todo text',
     });
     return;
   }
-  const prevId = todoList.reduce((prev, curr) => {
-    return prev > curr.id ? prev : curr.id;
-  }, 0);
-  const newTodo = {
-    id: prevId + 1,
-    todo: req.body.todo,
-  };
-  todoList.push(newTodo);
-  res.json(newTodo);
+  db.Todo.create({
+    name: req.body.name
+  })
+    .then((newTodo) => {
+      res.json(newTodo);
+    })
+    .catch((error => {
+      console.error(error);
+      res.status(500).json({error: 'A Database Error Occurred' });
+    }))
 });
+
+
+
 
 // PUT /api/todos/:id
 app.put('/api/todos/:id', (req, res) => {
-  if (!req.body || !req.body.todo) {
+  if (!req.body || !req.body.name) {
     res.status(400).json({
       error: 'Provide todo text',
     });
     return;
   }
-  let updatedTodo = {};
-  todoList.forEach((todo) => {
-    if (todo.id === Number.parseInt(req.params.id)) {
-      todo.todo = req.body.todo;
-      updatedTodo = todo;
-    }
-  });
-  const status = Object.keys(updatedTodo).length ? 200 : 404;
-  res.status(status).json(updatedTodo);
+  const { id } = req.params;
+  db.Todo.findByPk(id)
+    .then((todo) => {
+      if (!todo) {
+        res.status(404).json({ error: `Could not find Todo with id: ${id}`})
+        return;
+      }
+      todo.name = req.body.name;
+      todo.save()
+      res.json(todo);
+    })
+    .catch((error => {
+      console.error(error);
+      res.status(500).json({error: 'A Database Error Occurred' });
+    }))
 });
 
 // DELETE /api/todos/:id
 app.delete('/api/todos/:id', (req, res) => {
-  let found = false;
-  todoList = todoList.filter((todo) => {
-    if (todo.id === Number.parseInt(req.params.id)) {
-      found = true;
-      return false;
+  const { id } = req.params;
+  db.Todo.destroy({
+    where: {
+      id: req.params.id
     }
-    return true;
-  });
-  const status = found ? 200 : 404;
-  res.status(status).json(todoList);
+  })
+  .then((deleted) => {
+    if (deleted === 0) {
+      res.status(404).json({ error: `Could not find Todo with id: ${id}`})
+      return
+    }
+    res.status(204).json()
+  })
+  .catch((error => {
+    console.error(error);
+    res.status(500).json({error: 'A Database Error Occurred' });
+  }))
 });
 
 app.listen(3000, function () {
