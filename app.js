5ab27e57beee7da6e95771a5849e54e45be719c4
Nina Kim <ninakim0504@hotmail.com>
2020-10-12 22:56:57 -0400

    newfiles

--- app.js
+++ app.js
@@ -1,11 +1,91 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db =  require('./models')
+const es6Renderer = require('express-es6-template-engine');
+const bcrypt = require('bcrypt');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('./public'));
+
+app.engine('html', es6Renderer); // use es6renderer for html view templates
+app.set('views', 'templates'); // look in the 'templates' folder for view templates
+app.set('view engine', 'html'); // set the view engine to use the 'html' views
+
+app.get('/register', (req, res) => {
+  res.render('register', {
+    locals: {
+      error: null,
+      }
+    });
+  })
+
+app.post('/register', (req, res) => {
+  //check if post was submitted with email and password
+if (!req.body.email || !req.body.password) {
+  res.render('register', {
+    locals: {
+      error: 'Please submit all required fields'
+      }
+    })
+    return;
+  }
+  const{ email, password } = req.body;
+  bcrypt.hash(password, 10, (err, hash) => {
+    db.User.create ({
+      email: email,
+      password: hash,
+    })
+    .then((user) => {
+      res.redirect('/login');
+    })
+  })
+})
+
+app.get('/login', (req, res) => {
+  res.render('login', {
+  locals: {
+    error: null,
+    }
+  });
+})
+
+app.post('/login', (req, res) => {
+  //check if post was submitted with email and password
+  if (!req.body.email || !req.body.password) {
+    res.render('login', {
+      locals: {
+        error: 'Please submit all required fields'
+        }
+      })
+      return;
+    }
+    db.User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+    .then((user) => {
+      if (!user) {
+        res.render('login', {
+          locals: {
+            error: 'No user with that email'
+          }
+        })
+        return;
+      }
+      bcrypt.compare(req.body.password, user.password, (err, matched) => {
+        if(matched) {
+          res.send('YOU LOGGED IN')
+        } else {
+          res.send('NOPE, TRY AGAIN: WRONG PASSWORD')
+        }
+        return;
+      })
+    })
+  })
 
 let todoList = [
   {
--- public/script.js
+++ public/script.js
@@ -164,5 +164,16 @@ document.addEventListener('click', (e) => {
   }
 });
 
+$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+
 // render the todos from the server onto the page
 renderTodos();
